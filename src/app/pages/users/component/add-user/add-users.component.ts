a943c72fdb91e73fcba7816ba416e5a061511ea5
Manprit Tiwari <manpreet.mobicloud@gmail.com>
2022-11-18 15:54:43 +0530

    fixed add user Form

--- src/app/pages/users/component/add-user/add-users.component.ts
+++ src/app/pages/users/component/add-user/add-users.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder,FormControl,FormGroup } from '@angular/forms';
+import { FormBuilder,FormControl,FormGroup, Validators } from '@angular/forms';
 import { FloatLabelType } from '@angular/material/form-field';
 @Component({
   selector: 'app-add-users',
@@ -7,19 +7,49 @@ import { FloatLabelType } from '@angular/material/form-field';
   styleUrls: ['./add-users.component.scss']
 })
 export class AddUsersComponent implements OnInit {
+showPassword: any;
+
+
+
+  constructor() { }
+  
+  userForm = new FormGroup<any>({
+    'firstName': new FormControl('', Validators.required),
+    'lastName': new FormControl('', Validators.required),
+    'mobileNo': new FormControl('', Validators.required),
+    'dateOfBirth': new FormControl('', Validators.required),
+    'designation': new FormControl('', Validators.required),
+    'role': new FormControl('', Validators.required),
+    'address': new FormControl('', Validators.required),
+    'userName': new FormControl('', [Validators.required, Validators.email]),
+    'password': new FormControl('', Validators.required)
+  })
+
+
   ngOnInit(): void {
-    
+    this.watchFormForChanges();
   }
-  hideRequiredControl = new FormControl(false);
-  floatLabelControl = new FormControl('always' as FloatLabelType);
- 
 
-  constructor(private _formBuilder: FormBuilder) {}
+  get role() {
+    return this.userForm.get('role');
+  }
 
-  getFloatLabelValue(): FloatLabelType {
-    return this.floatLabelControl.value || 'always';
+  watchFormForChanges = () => {
+    this.role?.valueChanges.subscribe(() => {
+      if (this.role?.value === 'sales') {
+        this.userForm.addControl('salesHead', new FormControl(''));
+      }
+      else {
+        if (this.userForm.get('salesHead')) {
+          this.userForm.removeControl('salesHead');
+        }
+      }
+    })
   }
-  submit(){
-    console.log("submitted");
+
+  onSubmit = () => {
+
   }
+  
+  
 }
