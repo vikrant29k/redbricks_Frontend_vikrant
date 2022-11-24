2e3923d4514eb11dcf41f92eeafc65ed9d366561
vikrant29k <116786939+vikrant29k@users.noreply.github.com>
2022-11-24 12:49:01 +0530

    some spelling changes from bug sheets

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   // baseUrl: 'http://localhost:3000/'
-  baseUrl: 'https://redbricks-server.herokuapp.com/'
+  // baseUrl: 'https://redbricks-server.herokuapp.com/'
+  baseUrl: 'http://192.168.29.112:3000/'
 };
